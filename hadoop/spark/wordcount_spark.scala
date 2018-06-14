c800fdfd4daffb6d4a4afab26a6d7768d8aa09a9
assistbigdata <assistbigdata7@gmail.com>
2018-06-15 00:44:17 +0900

    Create wordcount_spark.scala

---
+++ hadoop/spark/wordcount_spark.scala
@@ -0,0 +1,7 @@
+sc.textFile("s3://kr.assist.2018.emr/data/obama.txt")
+    .flatMap(_.split("\\s+"))
+    .map(_.toLowerCase.replaceAll("\\W",""))
+    .filter(_.size >0)     
+    .groupBy(identity)
+    .map(x => x._2.size -> x._1)
+    .sortByKey(false,1).foreach(println)
